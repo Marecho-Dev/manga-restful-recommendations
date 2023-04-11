e0ffd5cc9695b4540b4eceaeafa93072ceee7305
Marek <marekstaszkiewicz@live.com>
2023-04-11 11:22:36 -0400

    added logging

--- optimized_manga_rec.py
+++ optimized_manga_rec.py
@@ -17,7 +17,7 @@ from scipy.sparse import csr_matrix, hstack, csc_matrix
 import os
 process = psutil.Process()
 mem_info = process.memory_info()
-print(f"Current memory usage: {mem_info.rss / 1024 / 1024} MB")
+logging.info(f"Current memory usage: {mem_info.rss / 1024 / 1024} MB")
 
 db = Prisma(
     http={
@@ -71,7 +71,7 @@ async def user_recommendation(user_id, m_value=None):
     # Create a mapping of user_id and manga_id to indices
     user_id_map = {user_id: i for i, user_id in enumerate(main_df['user_id'].unique())}
     manga_id_map = {manga_id: i for i, manga_id in enumerate(main_df['manga_id'].unique())}
-    print(f"Current memory usage: {mem_info.rss / 1024 / 1024} MB")
+    logging.info(f"Current memory usage: {mem_info.rss / 1024 / 1024} MB")
     # Compute the rows, columns, and data for the CSR matrix
     rows = main_df['user_id'].map(user_id_map.get).values
     cols = main_df['manga_id'].map(manga_id_map.get).values
@@ -81,10 +81,10 @@ async def user_recommendation(user_id, m_value=None):
     main_piv_sparse = csr_matrix((data, (rows, cols)), shape=(len(user_id_map), len(manga_id_map)))
 
     manga_similarity = cosine_similarity(main_piv_sparse)
-    print(manga_similarity)
+    logging.info(manga_similarity)
     # this gets pkled in my old file as manga pkl - this is the cosine similarity of similar users.
     manga_sim_df = pd.DataFrame(manga_similarity, index=user_ids, columns=user_ids)
-    print(manga_sim_df)
+    logging.info(manga_sim_df)
     number = 1
     similar_users = ""
     user_list = []
@@ -107,9 +107,9 @@ async def user_recommendation(user_id, m_value=None):
                     where user_id = '{user_id}'
                     ) 
                     and ( """
-    print(f"Current memory usage: {mem_info.rss / 1024 / 1024} MB")
+    logging.info(f"Current memory usage: {mem_info.rss / 1024 / 1024} MB")
     for user in manga_sim_df.sort_values(by=user_id, ascending=False).index[1:101]:
-        print(f'#{number}: {user}, {round(manga_sim_df[user][user_id] * 100, 2)}% match\n')
+        logging.info(f'#{number}: {user}, {round(manga_sim_df[user][user_id] * 100, 2)}% match\n')
         similar_users += f'#{number}: {user}, {round(manga_sim_df[user][user_id] * 100, 2)}% match\n'
         user_list.append(user)
         manga_query = manga_query + f"\n            user_id = '{user}'"
@@ -125,11 +125,11 @@ async def user_recommendation(user_id, m_value=None):
                 weighted_rating DESC;"""
 
         number += 1
-    print(manga_query)
-    print(f"Current memory usage: {mem_info.rss / 1024 / 1024} MB")
+    logging.info(manga_query)
+    logging.info(f"Current memory usage: {mem_info.rss / 1024 / 1024} MB")
     manga_recs = db.query_raw(manga_query)
-    print(manga_recs)
-    print(f"Current memory usage: {mem_info.rss / 1024 / 1024} MB")
+    logging.info(manga_recs)
+    logging.info(f"Current memory usage: {mem_info.rss / 1024 / 1024} MB")
     return manga_recs
 
 
